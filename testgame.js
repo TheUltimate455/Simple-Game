d3999bf8bc40422984fcebae602184911b64d4ee
TheUltimate455 <unathi.ndzinisa@gmail.com>
2023-12-12 19:07:31 +0200

    Modified

--- testgame.js
+++ testgame.js
@@ -1,1 +1,11 @@
-console.log("Unathi is here")
+const canvas - document.getElementById("canvas")
+const ctx - canvas.getContext("2d")
+
+let x = 0;
+let y = 0;
+
+function update(){
+	ctx.fillRect(0, 0, 50, 50)
+	requestAnimationFrame(update)
+}
+update()
